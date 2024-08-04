88899abb68e9e88900f6d3f0e5750be0e8f6a7de
tania <tania.inmareta-2023@fst.unair.ac.id>
2024-08-04 19:56:18 +0700

    Menambahkan file gambar dan stylesheet baru - Menambahkan file gambar bg.jpg di public/css - Menambahkan file stylesheet style.css di public/css - Memodifikasi file tugas2Aitc.blade.php di resources/views

--- resources/views/tugas2Aitc.blade.php
+++ resources/views/tugas2Aitc.blade.php
@@ -1,3 +1,24 @@
-<body>
-    <h1> Hello world</h1>
-</body>
+<!DOCTYPE html>
+<html>
+    <head> 
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login web Tania</title>
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+
+    <body>
+        <div class="login">
+            <form class="form">
+                <h1>Login</h1>
+                <label for="user">Username</label>
+                <input type="text" id="user" placeholder="Enter your username"><br>
+                <label for="pw">Password</label>
+                <input type="password" id="pw" placeholder="Enter your password"><br>
+                <p><a href="#">Forgot password?</a></p>
+                <button>LOG IN</button>
+                <p>Don't have an account? <a href="#">Register</a></p>
+            </form>
+        </div>
+    </body>
+</html>
